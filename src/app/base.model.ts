bbeb30a00ef995c52217d211ea8dbc3e80ec2a0c
Santiago Alvarez <santiagoalvarez9617@gmail.com>
2023-02-03 23:37:48 -0500

    Class 15: using 'readonly' to protect data

--- src/app/base.model.ts
+++ src/app/base.model.ts
@@ -1,6 +1,9 @@
 /* this is the base model that all of our other models are going to inherit*/
 export interface BaseModel {
-  id: string | number;
-  createdAt: Date;
+  /* in order to protect variables that we know their values will never change, we use the
+  reserved work 'readonly' before their declaration, like this: */
+  readonly id: string | number;
+  readonly createdAt: Date;
   updatedAt?: Date;
 }
+/* readonly can only be used with properties inside classes, interfaces and objects */
