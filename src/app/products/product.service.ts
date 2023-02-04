19ba9e4899d91541b8a4fe223550453fe85cc82f
Santiago Alvarez <santiagoalvarez9617@gmail.com>
2023-02-04 17:34:07 -0500

    Create functions to manage products array

--- src/app/products/product.service.ts
+++ src/app/products/product.service.ts
@@ -3,10 +3,6 @@ import { Product, Size } from "./product.model";
 
 export const products: Product[] = [];
 
-export const addProduct = (data: Product) => {
-  products.push(data)
-}
-
 export const sizesArr: Size[] = ['S', 'M', 'L', 'XL'];
 
 export const giveRandomNumber = (id: 'stock' | 'price') => {
@@ -15,4 +11,28 @@ export const giveRandomNumber = (id: 'stock' | 'price') => {
   } else {
     return faker.datatype.number({min: 20, max: 1500});
   }
+}
+
+export const addProduct = (data: Product) => {
+  products.push(data);
+}
+
+export const getProduct = (id: string) => {
+  return products.findIndex(product => product.id === id);
+}
+
+export const findProduct = (id: string) => {
+  const productIndex = getProduct(id);
+  return products[productIndex];
+}
+
+export const deleteProduct = (id: string) => {
+  const productToDelete = getProduct(id);
+  return products.splice(productToDelete, 1);
+}
+
+export const updateProduct = (id: string, changes: Product) => {
+  const productToChange = getProduct(id);
+  products.splice(productToChange, 1, changes);
+  return products[productToChange];
 }
