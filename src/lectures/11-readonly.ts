08681a442a3bceb76c2cd23b35f7ed14935cdcf4
Santiago Alvarez <santiagoalvarez9617@gmail.com>
2023-02-05 00:21:49 -0500

    fix: add one more file to last commit

---
+++ src/lectures/11-readonly.ts
@@ -0,0 +1,3 @@
+/* ReadonlyArray is used to make an array immutable. This also means that we can only use
+immutable array methods like map, forEach, filter, reduce, etc. */
+const numbers: ReadonlyArray<number> = [1,2,3,4];
